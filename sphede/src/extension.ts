e8b9e4be2db96bfbee51e64b6ea77594a5a7c68c
swarup <swarup@technative.in>
2023-10-20 13:05:28 +0530

    feat: add deploy functionality

--- sphede/src/extension.ts
+++ sphede/src/extension.ts
@@ -1,26 +1,96 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import {
+	SpheronClient,
+	ProtocolEnum,
+	ProviderEnum,
+	FrameworkEnum,
+	NodeVersionEnum,
+} from "@spheron/site";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const token =
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlLZXkiOiI4MGM0YTYxYWYwMWI3NzBkMDAyN2I3MDUxMDdjZDg1NWMzYTVhMmJmMjE4ZTE4NTAwMmE2YjEyNmUwN2Y0MTA3NjhjYzMxNWMxODE2YjM2NDg3NzZmZWJjOGIxNDI2MTQ4OTE1MzcwMTY1YmI1OWYyNGY1ZTVlNWZiZGFhNmQ0ZiIsImlhdCI6MTY5Nzc4NTQ4OCwiaXNzIjoid3d3LnNwaGVyb24ubmV0d29yayJ9.mKYo8La_Ui4K9KI5Opvn0biTR-kLcuMjFJEcVmI9vNI";
+
+const spheronClient = new SpheronClient({ token });
+
+console.log(spheronClient);
+
 export function activate(context: vscode.ExtensionContext) {
+	const disposable = vscode.commands.registerCommand("sphede", async () => {
+		// const projectName = await vscode.window.showInputBox({
+		// 	prompt: "Enter the project name",
+		// 	placeHolder: "MyProject",
+		// });
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sphede" is now active!');
+		// if (!projectName) {
+		// 	vscode.window.showErrorMessage("Project name is required.");
+		// 	return;
+		// }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('sphede.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from sphede!');
+		// const githubUrl = await vscode.window.showInputBox({
+		// 	prompt: "Enter the GitHub URL",
+		// 	placeHolder: "https://github.com/SwarupKharul/Tic-Tac-Toe",
+		// });
+
+		// if (!githubUrl) {
+		// 	vscode.window.showErrorMessage("GitHub URL is required.");
+		// 	return;
+		// }
+
+		// Create a progress loader while deploying
+		await vscode.window.withProgress(
+			{
+				location: vscode.ProgressLocation.Notification,
+				title: "Deploying project...",
+				cancellable: true,
+			},
+			async (progress, t) => {
+				t.onCancellationRequested(() => {
+					// Handle cancellation
+					vscode.window.showErrorMessage("Deployment canceled.");
+				});
+
+				try {
+					let response = await spheronClient.deployments.deploy({
+						gitUrl: "https://github.com/SwarupKharul/Tic-Tac-Toe",
+						projectName: "test",
+						environmentVariables: {
+							key1: "value1",
+						},
+						provider: ProviderEnum.GITHUB,
+						branch: "main",
+						protocol: ProtocolEnum.FILECOIN,
+						configuration: {
+							framework: FrameworkEnum.REACT,
+							workspace: "",
+							installCommand: "yarn install",
+							buildCommand: "yarn build",
+							publishDir: "build",
+							nodeVersion: NodeVersionEnum.V_16,
+						},
+						gitProviderPreferences: {
+							prComments: false,
+							commitComments: false,
+							buildStatus: false,
+							githubDeployment: true,
+						},
+					});
+
+					let deployementUrl = await spheronClient.deployments.get(
+						"653227431a79370012cf7cdb"
+					);
+
+					console.log(deployementUrl);
+					vscode.window.showInformationMessage("Deployment successful.");
+				} catch (error) {
+					vscode.window.showErrorMessage(
+						`Deployment failed: ${error as Error}`
+					);
+				}
+			}
+		);
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
