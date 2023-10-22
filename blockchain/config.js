81ba3c2d1cf52e368e6be9f5295627242e09c062
Pranav Desai <74852751+pranavvdesai@users.noreply.github.com>
2023-10-22 21:09:36 +0530

    Update config.js

--- blockchain/config.js
+++ blockchain/config.js
@@ -1,2 +1,6 @@
 export const contractAddress = "0xE291bC33160BBcd584FfE2A5727c7E6b0B9886FC";
 export const kaamifyAddress = "0x32F055AeF46ee2E2997758F26c6BDDd279E41DF3";
+
+// deployed on scroll network 
+// export const contractAddress = "0x0F8248fbEb9843817B421F36c090F2A266Bd8132";
+// export const kaamifyAddress = "0xA6731dF3Ae5B453b247Df5615c0d092345C78FE2";
